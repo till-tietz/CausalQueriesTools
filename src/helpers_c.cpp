b0b4e4dcc4b24c5ce5219d0377ed205786576291
till-tietz <ttietz2014@gmail.com>
2022-12-09 12:17:03 +0100

    cpp implementation for map_query realise_outcomes functionality

--- src/helpers_c.cpp
+++ src/helpers_c.cpp
@@ -148,3 +148,81 @@ void realise_outcomes_c(SEXP outcomes,
 
   return;
 }
+
+
+//[[Rcpp::export]]
+void realise_outcomes_singular_c(SEXP outcomes,
+                                 std::vector<std::string> nodes,
+                                 std::vector<std::string> endogenous_nodes,
+                                 List dos,
+                                 List parents_list,
+                                 List nodal_types,
+                                 List nodal_types_colnames,
+                                 List nodal_types_collapsed,
+                                 int n_causal_types,
+                                 std::vector<std::string> vars){
+
+
+ //connect to bigmat
+ Rcpp::XPtr<BigMatrix> outcomes_mat(outcomes);
+ MatrixAccessor<int> outcomes_mat_access(*outcomes_mat);
+
+ std::vector<std::string> in_dos = dos.names();
+
+ for(int i = 0; i < dos.size(); ++i){
+   // generate causal types and fill in dos
+   std::vector<std::vector<std::string>> ct = make_causal_types_c(nodal_types_collapsed);
+   std::string in_dos_i = in_dos[i];
+   int pos = std::find(nodes.begin(), nodes.end(), in_dos_i) - nodes.begin();
+   int dos_i_int = dos[in_dos_i];
+   std::vector<std::string> dos_i;
+   dos_i.push_back(std::to_string(dos_i_int));
+   ct[pos] = rep_times(dos_i, n_causal_types);
+
+   // loop over each endogenous node
+   for(int j = 0; j < endogenous_nodes.size(); ++j){
+     std::string var = endogenous_nodes[j];
+     int pos = std::find(nodes.begin(), nodes.end(), var) - nodes.begin();
+     //get causal type realizations for endogenous node
+     std::vector<std::string> child_type = ct[pos];
+     //get parents of endogenous node
+     std::vector<std::string> parents = parents_list[var];
+     //get nodal types of endogenous node
+     std::vector<std::string> nodal_label = nodal_types_collapsed[var];
+     //get uncollapsed nodal types for endogenous node
+     arma::mat nodal_type_var = nodal_types[var];
+     //get uncollapsed nodal types colnames
+     std::vector<std::string> nodal_type_var_col = nodal_types_colnames[var];
+
+     //loop over causal types
+     for(int k = 0; k < child_type.size(); ++k){
+       //get causal type
+       std::string type = child_type[k];
+       //generate empty vector for parent realization
+       std::string parents_val;
+       //get parent realization
+       for(int l = 0; l < parents.size(); ++l){
+         int pos_parent = std::find(nodes.begin(), nodes.end(), parents[l]) - nodes.begin();
+         parents_val.append(ct[pos_parent][k]);
+       }
+       //find row position of type
+       int row = std::find(nodal_label.begin(),nodal_label.end(), type) - nodal_label.begin();
+       //find realization and add to J
+       int pos_col = std::find(nodal_type_var_col.begin(), nodal_type_var_col.end(), parents_val) - nodal_type_var_col.begin();
+       int outcome = int(nodal_type_var(row,pos_col));
+       ct[pos][k] = std::to_string(outcome);
+     }
+   }
+
+   std::string var_i = vars[i];
+   pos = std::find(nodes.begin(), nodes.end(), var_i) - nodes.begin();
+
+   for(int m = 0; m < n_causal_types; ++m){
+     outcomes_mat_access[i][m] = std::stoi(ct[pos][m]);
+   }
+ }
+
+ return;
+
+}
+
