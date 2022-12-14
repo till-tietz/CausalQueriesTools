9546ac27e14aae7451f5bbdbee3f562a43a44d37
till-tietz <ttietz2014@gmail.com>
2022-12-14 20:02:30 +0100

    fixed query_to_ct_c bug

--- src/helpers_c.cpp
+++ src/helpers_c.cpp
@@ -420,40 +420,49 @@ std::vector<std::vector<int>> query_to_ct_c(
       std::vector<std::string> dos_i;
       dos_i.push_back(std::to_string(dos_i_int));
       ct[pos] = rep_times(dos_i, n_causal_types);
+    } else {
+      in_dos_i = "";
+    }
 
-      // loop over each endogenous node
-      for(int j = 0; j < endogenous_nodes.size(); ++j){
-        std::string var = endogenous_nodes[j];
-        int pos = std::find(nodes.begin(), nodes.end(), var) - nodes.begin();
-        //get causal type realizations for endogenous node
-        std::vector<std::string> child_type = ct[pos];
-        //get parents of endogenous node
-        std::vector<std::string> parents = parents_list[var];
-        //get nodal types of endogenous node
-        std::vector<std::string> nodal_label = nodal_types_collapsed[var];
-        //get uncollapsed nodal types for endogenous node
-        arma::mat nodal_type_var = nodal_types[var];
-        //get uncollapsed nodal types colnames
-        std::vector<std::string> nodal_type_var_col = nodal_types_colnames[var];
+    std::vector<std::string> work_through;
+    for(int n = 0; n < endogenous_nodes.size(); ++n){
+      if(endogenous_nodes[n] != in_dos_i){
+        work_through.push_back(endogenous_nodes[n]);
+      }
+    }
 
-        //loop over causal types
-        for(int k = 0; k < child_type.size(); ++k){
-          //get causal type
-          std::string type = child_type[k];
-          //generate empty vector for parent realization
-          std::string parents_val;
-          //get parent realization
-          for(int l = 0; l < parents.size(); ++l){
-            int pos_parent = std::find(nodes.begin(), nodes.end(), parents[l]) - nodes.begin();
-            parents_val.append(ct[pos_parent][k]);
-          }
-          //find row position of type
-          int row = std::find(nodal_label.begin(),nodal_label.end(), type) - nodal_label.begin();
-          //find realization and add to J
-          int pos_col = std::find(nodal_type_var_col.begin(), nodal_type_var_col.end(), parents_val) - nodal_type_var_col.begin();
-          int outcome = int(nodal_type_var(row,pos_col));
-          ct[pos][k] = std::to_string(outcome);
+    // loop over each endogenous node
+    for(int j = 0; j < work_through.size(); ++j){
+      std::string var = work_through[j];
+      int pos = std::find(nodes.begin(), nodes.end(), var) - nodes.begin();
+      //get causal type realizations for endogenous node
+      std::vector<std::string> child_type = ct[pos];
+      //get parents of endogenous node
+      std::vector<std::string> parents = parents_list[var];
+      //get nodal types of endogenous node
+      std::vector<std::string> nodal_label = nodal_types_collapsed[var];
+      //get uncollapsed nodal types for endogenous node
+      arma::mat nodal_type_var = nodal_types[var];
+      //get uncollapsed nodal types colnames
+      std::vector<std::string> nodal_type_var_col = nodal_types_colnames[var];
+
+      //loop over causal types
+      for(int k = 0; k < child_type.size(); ++k){
+        //get causal type
+        std::string type = child_type[k];
+        //generate empty vector for parent realization
+        std::string parents_val;
+        //get parent realization
+        for(int l = 0; l < parents.size(); ++l){
+          int pos_parent = std::find(nodes.begin(), nodes.end(), parents[l]) - nodes.begin();
+          parents_val.append(ct[pos_parent][k]);
         }
+        //find row position of type
+        int row = std::find(nodal_label.begin(),nodal_label.end(), type) - nodal_label.begin();
+        //find realization and add to J
+        int pos_col = std::find(nodal_type_var_col.begin(), nodal_type_var_col.end(), parents_val) - nodal_type_var_col.begin();
+        int outcome = int(nodal_type_var(row,pos_col));
+        ct[pos][k] = std::to_string(outcome);
       }
     }
 
